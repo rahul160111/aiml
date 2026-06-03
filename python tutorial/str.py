df8fa8caa80c69d72e8dde37a39a150159abeb07
rahul160111 <rahulpnchl07@gmail.com>
2026-06-03 16:09:06 +0530

    string

--- python tutorial/str.py
+++ python tutorial/str.py
@@ -1,1 +1,35 @@
 a = '2345.6'
+#   -5 -4 -3 -2 -1
+
+# 012345
+b = "water is very clear"
+print(b)
+
+r = b.replace("clear","dirty")
+print(b)
+print(b.strip())
+
+c = " hey rahul a first party git hub outh application cridential manager with gist repo and world flow scopes was recently authorized to access your account "
+
+# count length of variable a 
+print (len(a))
+
+# print(a.upper())
+# print(a.lower())
+# print(a.title())
+# print ()
+print(c.capitalize())
+
+# print(a.istitle())
+# print(a.islower())
+# print(a.isupper())
+x = a.isdecimal()
+print(x)
+ 
+# print(a.find("x"))
+# print(a.index("x"))
+r = b.replace("clear","dirty")
+print(r) 
+
+print(b)
+print(b.strip())
