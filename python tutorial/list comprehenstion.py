f91fc6d57738f45e73e1fbcf84a40167fc1a4f02
rahul160111 <rahulpnchl07@gmail.com>
2026-06-10 16:38:36 +0530

    string

---
+++ python tutorial/list comprehenstion.py
@@ -0,0 +1,20 @@
+l = [ -20,-19,-18,-17,-16,-15,-14,-13,-12,-11,-10,-9,-8,-7,-6,-5,-4,-3,-2,-1-0,
+     1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20 ]
+
+for i in  l:
+    if i%2==0:
+        k = [i]
+        print(k)
+
+# L = [i for i in range (-20,20) if i%2==0]
+# print(L)
+
+L = (i for i in range (-20,20)if i%2==0)
+print(tuple(L))
+
+# L = { i for i in range (-20,20)if i%2==0}
+# print(L)
+
+# L = { i:i**4 for i in range (-20,20)if i%2==0}
+# print(L)
+
--- python tutorial/tempCodeRunnerFile.py
+++ python tutorial/tempCodeRunnerFile.py
@@ -1,1 +1,2 @@
-",n*x)
+L =  (i for i in  range (-20,20)if i%2==o)
+print(tuple(L))
